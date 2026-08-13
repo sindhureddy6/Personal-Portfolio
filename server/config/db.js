a167d7ad96c966c22bc70d5cfaced4a674763676
sindhu <sindhureddy5226@gmail.com>
2026-08-13 16:00:57 +0530

    fix mysql connection for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,23 @@
 const mysql=require("mysql2");
 require("dotenv").config();
 
-const db=mysql.createConnection({
+const db=mysql.createPool({
     host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+    database:process.env.DB_NAME,
+    waitForConnections:true,
+    connectionLimit:10,
+    queueLimit:0
 });
 
-db.connect((err)=>{
+db.getConnection((err,connection)=>{
     if(err){
         console.log("Database connection failed:",err);
     }else{
         console.log("Database connection established successfully");
+        connection.release();
     }
 });
 
